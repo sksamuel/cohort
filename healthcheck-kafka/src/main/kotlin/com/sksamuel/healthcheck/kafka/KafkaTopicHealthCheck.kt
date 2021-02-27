8e077c61398c34ef08189fa565396e604ad83c15
sksamuel <sam@sksamuel.com>
2021-02-27 17:06:20 -0600

    Added KafkaTopicHealthCheck

--- healthcheck-kafka/src/main/kotlin/com/sksamuel/healthcheck/kafka/KafkaTopicHealthCheck.kt
+++ healthcheck-kafka/src/main/kotlin/com/sksamuel/healthcheck/kafka/KafkaTopicHealthCheck.kt
@@ -8,17 +8,24 @@ import java.util.Properties
 import java.util.concurrent.TimeUnit
 
 /**
- * A [HealthCheck] that checks that a connection can be made to a kafka cluster.
+ * A [HealthCheck] that checks that a topic exists on a kafka cluster.
  */
-class KafkaHealthCheck(private val bootstrapServers: String, private val ssl: Boolean) : HealthCheck {
+class KafkaTopicHealthCheck(
+  private val bootstrapServers: String,
+  private val ssl: Boolean,
+  private val topic: String
+) : HealthCheck {
   override fun check(): HealthCheckResult {
     val props = Properties()
     props[AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapServers
     if (ssl) props[AdminClientConfig.SECURITY_PROTOCOL_CONFIG] = "SSL"
     return try {
       val client = AdminClient.create(props)
-      client.describeCluster().controller().get(1, TimeUnit.MINUTES)
-      HealthCheckResult.Healthy
+      val desc = client.describeTopics(listOf(topic)).all().get(1, TimeUnit.MINUTES)[topic]
+      if (desc == null)
+        HealthCheckResult.Unhealthy("Topic $topic does not exist on kafka cluster $bootstrapServers", null)
+      else
+        HealthCheckResult.Healthy
     } catch (t: Throwable) {
       HealthCheckResult.Unhealthy("Could not connect to kafka cluster at $bootstrapServers", t)
     }
