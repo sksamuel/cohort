155cccab9472ab034efb2979b21623f5729436b4
sksamuel <sam@sksamuel.com>
2021-02-27 13:28:23 -0600

    Project bootstrap

---
+++ build.gradle.kts
@@ -0,0 +1,59 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+
+buildscript {
+   repositories {
+      jcenter()
+      mavenCentral()
+      maven {
+         url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+      }
+      maven {
+         url = uri("https://plugins.gradle.org/m2/")
+      }
+   }
+}
+
+plugins {
+   java
+   `java-library`
+   id("java-library")
+   id("maven-publish")
+   signing
+   maven
+   `maven-publish`
+   kotlin("jvm").version(Libs.kotlinVersion)
+}
+
+allprojects {
+   apply(plugin = "org.jetbrains.kotlin.jvm")
+
+   repositories {
+      mavenLocal()
+      mavenCentral()
+      maven {
+         url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+      }
+   }
+
+   group = Libs.org
+   version = Ci.version
+
+   dependencies {
+      testImplementation(Libs.Kotest.assertions)
+      testImplementation(Libs.Kotest.junit5)
+      implementation(Libs.Kotlin.stdlib)
+   }
+
+   tasks.named<Test>("test") {
+      useJUnitPlatform()
+      testLogging {
+         showExceptions = true
+         showStandardStreams = true
+         exceptionFormat = TestExceptionFormat.FULL
+      }
+   }
+
+   tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+      kotlinOptions.jvmTarget = "1.8"
+   }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,5 @@
+enableFeaturePreview("GRADLE_METADATA")
+
+include("healthcheck-core")
+include("healthcheck-kafka")
+include("healthcheck-jdbc")
