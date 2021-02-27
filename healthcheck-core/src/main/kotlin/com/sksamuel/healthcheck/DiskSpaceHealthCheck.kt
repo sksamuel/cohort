a4310ab9974d6f4c277d6eea53e1b61bc6c99838
sksamuel <sam@sksamuel.com>
2021-02-27 17:34:30 -0600

    Added DiskSpaceHealthCheck

---
+++ healthcheck-core/src/main/kotlin/com/sksamuel/healthcheck/DiskSpaceHealthCheck.kt
@@ -0,0 +1,27 @@
+package com.sksamuel.healthcheck
+
+import java.nio.file.FileStore
+import java.nio.file.FileSystems
+
+class DiskSpaceHealthCheck(
+  private val fileStore: FileStore,
+  private val minFreeSpacePercentage: Double = 10.0
+) : HealthCheck {
+
+  override fun check(): HealthCheckResult {
+    return try {
+      val availablePercent = fileStore.usableSpace / fileStore.totalSpace
+      if (availablePercent < minFreeSpacePercentage)
+        HealthCheckResult.Unhealthy("Disk space has dropped to $availablePercent on ${fileStore.name()}", null)
+      else
+        HealthCheckResult.Healthy("Disk space is $availablePercent on ${fileStore.name()}")
+    } catch (t: Throwable) {
+      HealthCheckResult.Unhealthy("Error querying disk space on ${fileStore.name()}", t)
+    }
+  }
+
+  companion object {
+    fun defaults(minFreeSpacePercentage: Double = 10.0): List<HealthCheck> =
+      FileSystems.getDefault().fileStores.map { DiskSpaceHealthCheck(it, minFreeSpacePercentage) }
+  }
+}
