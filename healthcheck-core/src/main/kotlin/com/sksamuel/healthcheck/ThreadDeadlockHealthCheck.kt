8ec0b9a82d6e3c93bf545e6f19b6f553b1ca4eff
sksamuel <sam@sksamuel.com>
2021-02-28 05:09:34 -0600

    Fixed thread deadlock checker

--- healthcheck-core/src/main/kotlin/com/sksamuel/healthcheck/ThreadDeadlockHealthCheck.kt
+++ healthcheck-core/src/main/kotlin/com/sksamuel/healthcheck/ThreadDeadlockHealthCheck.kt
@@ -1,13 +1,12 @@
 package com.sksamuel.healthcheck
 
 import java.lang.management.ManagementFactory
+import java.lang.management.ThreadMXBean
 
-class ThreadDeadlockHealthCheck : HealthCheck {
-
-  private val threads = ManagementFactory.getThreadMXBean()
+class ThreadDeadlockHealthCheck(private val bean: ThreadMXBean = ManagementFactory.getThreadMXBean()) : HealthCheck {
 
   override suspend fun check(): HealthCheckResult {
-    val threads = threads.findDeadlockedThreads()
+    val threads = bean.findDeadlockedThreads() ?: longArrayOf()
     val msg = "There are $threads deadlocked threads"
     return if (threads.isEmpty()) HealthCheckResult.Healthy(msg) else HealthCheckResult.Unhealthy(msg, null)
   }
