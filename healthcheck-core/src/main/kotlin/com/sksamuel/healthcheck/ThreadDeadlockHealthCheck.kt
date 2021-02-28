40a2a668294082739df431a086821846c1695d3c
sksamuel <sam@sksamuel.com>
2021-02-28 05:18:23 -0600

    Fixed thread deadlock count

--- healthcheck-core/src/main/kotlin/com/sksamuel/healthcheck/ThreadDeadlockHealthCheck.kt
+++ healthcheck-core/src/main/kotlin/com/sksamuel/healthcheck/ThreadDeadlockHealthCheck.kt
@@ -6,8 +6,8 @@ import java.lang.management.ThreadMXBean
 class ThreadDeadlockHealthCheck(private val bean: ThreadMXBean = ManagementFactory.getThreadMXBean()) : HealthCheck {
 
   override suspend fun check(): HealthCheckResult {
-    val threads = bean.findDeadlockedThreads() ?: longArrayOf()
-    val msg = "There are $threads deadlocked threads"
-    return if (threads.isEmpty()) HealthCheckResult.Healthy(msg) else HealthCheckResult.Unhealthy(msg, null)
+    val deadlocked = bean.findDeadlockedThreads()?.size ?: 0
+    val msg = "There are $deadlocked deadlocked threads"
+    return if (deadlocked == 0) HealthCheckResult.Healthy(msg) else HealthCheckResult.Unhealthy(msg, null)
   }
 }
