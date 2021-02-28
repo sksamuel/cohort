4be969d9b6fa8f70e13f2b36fea4f27d7942a8fe
sksamuel <sam@sksamuel.com>
2021-02-28 04:28:58 -0600

    Added names to health checks

--- healthcheck-core/src/main/kotlin/com/sksamuel/healthcheck/HealthCheckRegistry.kt
+++ healthcheck-core/src/main/kotlin/com/sksamuel/healthcheck/HealthCheckRegistry.kt
@@ -4,35 +4,39 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 
-class HealthCheckRegistry(private val healthchecks: List<HealthCheck>) {
+class HealthCheckRegistry(private val healthchecks: List<Pair<String, HealthCheck>>) {
 
-  fun register(healthCheck: HealthCheck): HealthCheckRegistry {
-    return HealthCheckRegistry(healthchecks + healthCheck)
+  fun register(name: String, healthCheck: HealthCheck): HealthCheckRegistry {
+    return HealthCheckRegistry(healthchecks + (name to healthCheck))
   }
 
   suspend fun execute(dispatcher: CoroutineDispatcher): HealthCheckResponse {
     val results = coroutineScope {
-      val jobs = healthchecks.map {
+      val jobs = healthchecks.map { (name, healthcheck) ->
         async(dispatcher) {
-          it.check()
+          try {
+            healthcheck.check()
+          } catch (t: Throwable) {
+            HealthCheckResult.Unhealthy("$name failed due to ${t.javaClass.name}", t)
+          }
         }
       }
       jobs.map { it.await() }
     }
-    val status = if (results.any { it is HealthCheckResult.Unhealthy }) HealthStatus.Green else HealthStatus.Red
+    val status = if (results.any { it is HealthCheckResult.Unhealthy }) HealthStatus.Unhealthy else HealthStatus.Healthy
     return HealthCheckResponse(status, results)
   }
 }
 
 enum class HealthStatus {
-  Green, Red
+  Healthy, Unhealthy
 }
 
 data class HealthCheckResponse(val status: HealthStatus, val results: List<HealthCheckResult>) {
-  fun <A> fold(ifRed: (List<HealthCheckResult>) -> A, ifGreen: (List<HealthCheckResult>) -> A): A {
+  fun <A> fold(ifUnhealthy: (List<HealthCheckResult>) -> A, ifHealthy: (List<HealthCheckResult>) -> A): A {
     return when (status) {
-      HealthStatus.Green -> ifGreen(results)
-      HealthStatus.Red -> ifRed(results)
+      HealthStatus.Healthy -> ifHealthy(results)
+      HealthStatus.Unhealthy -> ifUnhealthy(results)
     }
   }
 }
