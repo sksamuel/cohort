2d448f51942e8d602412d870d3285aad04107c70
sksamuel <sam@sksamuel.com>
2021-02-28 08:25:17 -0600

    Fixed memory mb calculation

--- healthcheck-core/src/main/kotlin/com/sksamuel/healthcheck/MemoryHealthCheck.kt
+++ healthcheck-core/src/main/kotlin/com/sksamuel/healthcheck/MemoryHealthCheck.kt
@@ -3,7 +3,7 @@ package com.sksamuel.healthcheck
 class MemoryHealthCheck(private val minFreeMb: Int) : HealthCheck {
   override fun check(): HealthCheckResult {
     val free = Runtime.getRuntime().freeMemory()
-    val freeMb = free * 1000_000
+    val freeMb = free / 1000_000
     return if (freeMb < minFreeMb) {
       HealthCheckResult.Unhealthy("Freemem is below threshold [$freeMb < $minFreeMb]", null)
     } else {
