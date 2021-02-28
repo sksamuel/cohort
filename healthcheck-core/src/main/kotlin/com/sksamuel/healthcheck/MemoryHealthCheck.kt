bbd9d212f711bac73e61063e004aed26966e8e3a
sksamuel <sam@sksamuel.com>
2021-02-28 07:10:55 -0600

    Added MemoryHealthCheck

---
+++ healthcheck-core/src/main/kotlin/com/sksamuel/healthcheck/MemoryHealthCheck.kt
@@ -0,0 +1,13 @@
+package com.sksamuel.healthcheck
+
+class MemoryHealthCheck(private val minFreeMb: Int) : HealthCheck {
+  override suspend fun check(): HealthCheckResult {
+    val free = Runtime.getRuntime().freeMemory()
+    val freeMb = free * 1000_000
+    return if (freeMb < minFreeMb) {
+      HealthCheckResult.Unhealthy("Freemem is below threshold [$freeMb < $minFreeMb]", null)
+    } else {
+      HealthCheckResult.Healthy("Freemem is above threshold [$freeMb >= $minFreeMb]")
+    }
+  }
+}
