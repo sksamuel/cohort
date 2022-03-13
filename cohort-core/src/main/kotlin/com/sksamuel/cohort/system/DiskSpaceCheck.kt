97524c76c7b08abe08865c132b77e605c8cc6845
sksamuel <sam@sksamuel.com>
2022-03-13 16:57:13 -0500

    Moved disk space check

--- cohort-core/src/main/kotlin/com/sksamuel/cohort/system/DiskSpaceCheck.kt
+++ cohort-core/src/main/kotlin/com/sksamuel/cohort/system/DiskSpaceCheck.kt
@@ -1,5 +1,7 @@
-package com.sksamuel.cohort
+package com.sksamuel.cohort.system
 
+import com.sksamuel.cohort.Check
+import com.sksamuel.cohort.CheckResult
 import java.nio.file.FileStore
 import java.nio.file.FileSystems
 import java.nio.file.Files
@@ -17,11 +19,11 @@ fun main() {
  *
  * For example, on linux, the default file system would typically have /, /sys, /proc and so on.
  *
- * To configure a check for the file store for the root directory, use [AvailableDiskSpaceCheck.root].
+ * To configure a check for the file store for the root directory, use [DiskSpaceCheck.root].
  *
  * The check is considered healthy if the percentage of free space is above [minFreeSpacePercentage].
  */
-class AvailableDiskSpaceCheck(
+class DiskSpaceCheck(
   private val fileStore: FileStore,
   private val minFreeSpacePercentage: Double = 10.0
 ) : Check {
@@ -41,18 +43,18 @@ class AvailableDiskSpaceCheck(
   companion object {
 
     /**
-     * Returns a [AvailableDiskSpaceCheck] for the root file store on the default file system.
+     * Returns a [DiskSpaceCheck] for the root file store on the default file system.
      * If there is more than one root directory available, an error will be thrown.
      */
-    fun root(minFreeSpacePercentage: Double = 10.0): AvailableDiskSpaceCheck {
+    fun root(minFreeSpacePercentage: Double = 10.0): DiskSpaceCheck {
       val root = FileSystems.getDefault().rootDirectories.single()
-      return AvailableDiskSpaceCheck(Files.getFileStore(root), minFreeSpacePercentage)
+      return DiskSpaceCheck(Files.getFileStore(root), minFreeSpacePercentage)
     }
 
     /**
-     * Returns a [AvailableDiskSpaceCheck] for each filestore in the get file system.
+     * Returns a [DiskSpaceCheck] for each filestore in the get file system.
      */
     fun defaults(minFreeSpacePercentage: Double = 10.0): List<Check> =
-      FileSystems.getDefault().fileStores.map { AvailableDiskSpaceCheck(it, minFreeSpacePercentage) }
+      FileSystems.getDefault().fileStores.map { DiskSpaceCheck(it, minFreeSpacePercentage) }
   }
 }
