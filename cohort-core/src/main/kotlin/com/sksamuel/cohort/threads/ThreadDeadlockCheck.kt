a6a42e94f23c7caa8e0b7ce78f4c246220e80388
sksamuel <sam@sksamuel.com>
2022-03-13 18:25:26 -0500

    Moved thread deadlock check

--- cohort-core/src/main/kotlin/com/sksamuel/cohort/threads/ThreadDeadlockCheck.kt
+++ cohort-core/src/main/kotlin/com/sksamuel/cohort/threads/ThreadDeadlockCheck.kt
@@ -1,9 +1,13 @@
-package com.sksamuel.cohort
+package com.sksamuel.cohort.threads
 
+import com.sksamuel.cohort.Check
+import com.sksamuel.cohort.CheckResult
 import java.lang.management.ManagementFactory
 import java.lang.management.ThreadMXBean
 
-class ThreadDeadlockCheck(private val bean: ThreadMXBean = ManagementFactory.getThreadMXBean()) : Check {
+class ThreadDeadlockCheck(
+  private val bean: ThreadMXBean = ManagementFactory.getThreadMXBean()
+) : Check {
 
   override suspend fun check(): CheckResult {
     val deadlocked = bean.findDeadlockedThreads()?.size ?: 0
