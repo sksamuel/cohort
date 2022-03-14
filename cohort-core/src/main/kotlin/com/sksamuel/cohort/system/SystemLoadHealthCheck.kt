5a1172a82c684a524fc3f832c65bc941ace30a80
sksamuel <sam@sksamuel.com>
2022-03-14 15:38:54 -0500

    System load average check #10

---
+++ cohort-core/src/main/kotlin/com/sksamuel/cohort/system/SystemLoadHealthCheck.kt
@@ -0,0 +1,26 @@
+package com.sksamuel.cohort.system
+
+import com.sksamuel.cohort.HealthCheck
+import com.sksamuel.cohort.HealthCheckResult
+import com.sun.management.OperatingSystemMXBean
+import java.lang.management.ManagementFactory
+
+/**
+ * A Cohort [HealthCheck] for the maximum system load.
+ *
+ * The check is considered healthy if the system load is < [maxLoad].
+ */
+class SystemLoadHealthCheck(private val maxLoad: Double) : HealthCheck {
+
+  private val bean = ManagementFactory.getOperatingSystemMXBean() as OperatingSystemMXBean
+
+  override suspend fun check(): HealthCheckResult {
+    val load = bean.systemLoadAverage
+    return if (load < maxLoad) {
+      HealthCheckResult.Healthy("System load is below threshold [$load < $maxLoad]")
+    } else {
+      HealthCheckResult.Unhealthy("System load is above threshold [$load >= $maxLoad]", null)
+    }
+  }
+
+}
