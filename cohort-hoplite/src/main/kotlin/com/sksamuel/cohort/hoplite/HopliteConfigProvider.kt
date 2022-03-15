7def0d1f50ab248321f2e1d985cd2cdaa796a5a2
sksamuel <sam@sksamuel.com>
2022-03-15 00:02:17 -0500

    Renamed HopliteConfigProvider

--- cohort-hoplite/src/main/kotlin/com/sksamuel/cohort/hoplite/HopliteConfigProvider.kt
+++ cohort-hoplite/src/main/kotlin/com/sksamuel/cohort/hoplite/HopliteConfigProvider.kt
@@ -3,7 +3,7 @@ package com.sksamuel.cohort.hoplite
 import com.sksamuel.cohort.config.ConfigProvider
 import com.sksamuel.hoplite.ConfigLoader
 
-class HopliteConfig(private val loader: ConfigLoader) : ConfigProvider {
+class HopliteConfigProvider(private val loader: ConfigLoader) : ConfigProvider {
   override suspend fun config(): Result<Any> = runCatching {
     return loader.loadConfigOrThrow()
   }
