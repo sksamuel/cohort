e62d2bf34ad8231a7fc1db89ec0a48f50c793704
sksamuel <sam@sksamuel.com>
2022-03-15 00:09:43 -0500

    Changed hoplite to return node

--- cohort-hoplite/src/main/kotlin/com/sksamuel/cohort/hoplite/HopliteConfigProvider.kt
+++ cohort-hoplite/src/main/kotlin/com/sksamuel/cohort/hoplite/HopliteConfigProvider.kt
@@ -2,9 +2,10 @@ package com.sksamuel.cohort.hoplite
 
 import com.sksamuel.cohort.config.ConfigProvider
 import com.sksamuel.hoplite.ConfigLoader
+import com.sksamuel.hoplite.Node
 
 class HopliteConfigProvider(private val loader: ConfigLoader) : ConfigProvider {
-  override suspend fun config(): Result<Any> = runCatching {
-    return loader.loadConfigOrThrow()
+  override suspend fun config(): Result<Node> = runCatching {
+    loader.loadNodeOrThrow()
   }
 }
