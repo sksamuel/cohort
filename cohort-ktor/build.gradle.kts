cae17c761f41b45e7c8da7713d79abb740945394
sksamuel <sam@sksamuel.com>
2022-03-14 23:29:26 -0500

    Fixed deps

--- cohort-ktor/build.gradle.kts
+++ cohort-ktor/build.gradle.kts
@@ -1,8 +1,8 @@
 dependencies {
    implementation(project(":cohort-core"))
    implementation("io.ktor:ktor-server-core:1.6.7")
-   implementation("com.fasterxml.jackson.core:jackson-core:2.13.2")
-   implementation("com.fasterxml.jackson.core:jackson-databind:2.13.2")
+   implementation("com.fasterxml.jackson.core:jackson-core:2.13.1")
+   implementation("com.fasterxml.jackson.core:jackson-databind:2.13.1")
    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
 }
 
