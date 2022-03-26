155676ceb274fea1b61636d613259146871ffe1a
sksamuel <sam@sksamuel.com>
2022-03-26 15:17:38 -0500

    Made peak threads mx bean configurable

--- cohort-core/src/main/kotlin/com/sksamuel/cohort/threads/PeakThreadsHealthCheck.kt
+++ cohort-core/src/main/kotlin/com/sksamuel/cohort/threads/PeakThreadsHealthCheck.kt
@@ -3,15 +3,17 @@ package com.sksamuel.cohort.threads
 import com.sksamuel.cohort.HealthCheck
 import com.sksamuel.cohort.HealthCheckResult
 import java.lang.management.ManagementFactory
+import java.lang.management.ThreadMXBean
 
 /**
  * A Cohort [HealthCheck] that the number of peak threads does not exceed a value.
  *
  * The check is considered healthy if the peak thread count <= [maxPeakThreads].
  */
-class PeakThreadsHealthCheck(private val maxPeakThreads: Int) : HealthCheck {
-
-  private val threadBean = ManagementFactory.getThreadMXBean()
+class PeakThreadsHealthCheck(
+  private val maxPeakThreads: Int,
+  private val threadBean: ThreadMXBean = ManagementFactory.getThreadMXBean(),
+) : HealthCheck {
 
   override suspend fun check(): HealthCheckResult {
     val count = threadBean.peakThreadCount
