db84c27074bb4f0a02dc08bd0889621475d50a75
sksamuel <sam@sksamuel.com>
2022-03-26 15:17:15 -0500

    Made max deadlocks and started threads bean configurable

--- cohort-core/src/main/kotlin/com/sksamuel/cohort/threads/StartedThreadsHealthCheck.kt
+++ cohort-core/src/main/kotlin/com/sksamuel/cohort/threads/StartedThreadsHealthCheck.kt
@@ -3,6 +3,7 @@ package com.sksamuel.cohort.threads
 import com.sksamuel.cohort.HealthCheck
 import com.sksamuel.cohort.HealthCheckResult
 import java.lang.management.ManagementFactory
+import java.lang.management.ThreadMXBean
 
 /**
  * A Cohort [HealthCheck] that the number of created and started threads does not exceed a value.
@@ -11,12 +12,13 @@ import java.lang.management.ManagementFactory
  *
  * The check is considered healthy if the started thread count <= [maxStartedThreads].
  */
-class StartedThreadsHealthCheck(private val maxStartedThreads: Int) : HealthCheck {
-
-  private val threadBean = ManagementFactory.getThreadMXBean()
+class StartedThreadsHealthCheck(
+  private val maxStartedThreads: Int,
+  private val threadMXBean: ThreadMXBean = ManagementFactory.getThreadMXBean(),
+) : HealthCheck {
 
   override suspend fun check(): HealthCheckResult {
-    val count = threadBean.totalStartedThreadCount
+    val count = threadMXBean.totalStartedThreadCount
     return if (count <= maxStartedThreads) {
       HealthCheckResult.Healthy("Started threads is below threshold [$count <= $maxStartedThreads]")
     } else {
--- cohort-core/src/main/kotlin/com/sksamuel/cohort/threads/ThreadDeadlockHealthCheck.kt
+++ cohort-core/src/main/kotlin/com/sksamuel/cohort/threads/ThreadDeadlockHealthCheck.kt
@@ -8,15 +8,16 @@ import java.lang.management.ThreadMXBean
 /**
  * A Cohort [HealthCheck] that checks for the presence of deadlocked threads.
  *
- * The check is considered healthy if the deadlock count is zero, and unhealthy otherwise.
+ * The check is considered healthy if the deadlock count is < [maxDeadlocks].
  */
 class ThreadDeadlockHealthCheck(
+  private val maxDeadlocks: Int = 1,
   private val bean: ThreadMXBean = ManagementFactory.getThreadMXBean()
 ) : HealthCheck {
 
   override suspend fun check(): HealthCheckResult {
     val deadlocked = bean.findDeadlockedThreads()?.size ?: 0
     val msg = "There are $deadlocked deadlocked threads"
-    return if (deadlocked == 0) HealthCheckResult.Healthy(msg) else HealthCheckResult.Unhealthy(msg, null)
+    return if (deadlocked < maxDeadlocks) HealthCheckResult.Healthy(msg) else HealthCheckResult.Unhealthy(msg, null)
   }
 }
