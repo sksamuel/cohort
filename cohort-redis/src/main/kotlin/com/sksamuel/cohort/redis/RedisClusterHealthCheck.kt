8afe706ff660714bd05d3d2f9edd200007101f4d
sksamuel <sam@sksamuel.com>
2022-04-04 23:37:03 -0500

    Updated redis healthcheck to allow easier customizable

--- cohort-redis/src/main/kotlin/com/sksamuel/cohort/redis/RedisClusterHealthCheck.kt
+++ cohort-redis/src/main/kotlin/com/sksamuel/cohort/redis/RedisClusterHealthCheck.kt
@@ -4,24 +4,27 @@ import com.sksamuel.cohort.HealthCheck
 import com.sksamuel.cohort.HealthCheckResult
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import redis.clients.jedis.DefaultJedisClientConfig
 import redis.clients.jedis.HostAndPort
-import redis.clients.jedis.JedisClientConfig
 import redis.clients.jedis.JedisCluster
+
+data class HostPort(val host: String, val port: Int)
 
 /**
  * A [HealthCheck] that checks that a connection can be made to a redis cluster.
- *
- * @param config provides access details to the cluster.
  */
 class RedisClusterHealthCheck(
-  private val hostsAndPorts: Set<HostAndPort>,
-  private val config: JedisClientConfig,
+  private val hostsAndPorts: Set<HostPort>,
+  private val username: String?,
+  private val password: String?,
+  private val tls: Boolean,
 ) : HealthCheck {
 
   override suspend fun check(): HealthCheckResult {
     return withContext(Dispatchers.IO) {
       runCatching {
-        val jedis = JedisCluster(hostsAndPorts, config)
+        val config = DefaultJedisClientConfig.builder().password(password).user(username).ssl(tls).build()
+        val jedis = JedisCluster(hostsAndPorts.map { HostAndPort(it.host, it.port) }.toSet(), config)
         jedis.use {
           when (val nodes = it.clusterNodes.size) {
             0 -> HealthCheckResult.Unhealthy("Connected to redis cluster but 0 nodes are available", null)
--- cohort-redis/src/main/kotlin/com/sksamuel/cohort/redis/RedisHealthCheck.kt
+++ cohort-redis/src/main/kotlin/com/sksamuel/cohort/redis/RedisHealthCheck.kt
@@ -4,28 +4,29 @@ import com.sksamuel.cohort.HealthCheck
 import com.sksamuel.cohort.HealthCheckResult
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import redis.clients.jedis.DefaultJedisClientConfig
 import redis.clients.jedis.HostAndPort
 import redis.clients.jedis.Jedis
-import redis.clients.jedis.JedisClientConfig
 
 /**
  * A [HealthCheck] that checks that a connection can be made to a redis instance.
- *
- * @param config provides access details to the cluster.
  */
 class RedisHealthCheck(
-  private val hostsAndPort: HostAndPort,
-  private val config: JedisClientConfig,
+  private val hostAndPort: HostPort,
+  private val username: String?,
+  private val password: String?,
+  private val tls: Boolean,
 ) : HealthCheck {
 
   override suspend fun check(): HealthCheckResult {
     return withContext(Dispatchers.IO) {
       runCatching {
-        val jedis = Jedis(hostsAndPort, config)
+        val config = DefaultJedisClientConfig.builder().password(password).user(username).ssl(tls).build()
+        val jedis = Jedis(HostAndPort(hostAndPort.host, hostAndPort.port), config)
         jedis.connection.use { it.ping() }
         HealthCheckResult.Healthy("Connected to redis cluster")
       }.getOrElse {
-        HealthCheckResult.Unhealthy("Could not connect to redis at $hostsAndPort", it)
+        HealthCheckResult.Unhealthy("Could not connect to redis at $hostAndPort", it)
       }
     }
   }
