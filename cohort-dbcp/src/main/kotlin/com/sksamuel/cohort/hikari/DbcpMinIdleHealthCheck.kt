64e064091f2013baf441456398206ddb01e35c41
sksamuel <sam@sksamuel.com>
2022-04-24 11:48:13 -0500

    Added DbcpMinIdleHealthCheck

---
+++ cohort-dbcp/src/main/kotlin/com/sksamuel/cohort/hikari/DbcpMinIdleHealthCheck.kt
@@ -0,0 +1,22 @@
+package com.sksamuel.cohort.hikari
+
+import com.sksamuel.cohort.HealthCheck
+import com.sksamuel.cohort.HealthCheckResult
+import org.apache.commons.dbcp2.BasicDataSource
+
+/**
+ * A Cohort [HealthCheck] that checks for the number of idle connections in a Apache DBCP2 [BasicDataSource].
+ *
+ * This is useful to detect when connections are being exhausted.
+ *
+ * The check is considered healthy if the idle count is >= [minIdle].
+ */
+class DbcpMinIdleHealthCheck(
+  private val ds: BasicDataSource,
+  private val minIdle: Int,
+) : HealthCheck {
+  override suspend fun check(): HealthCheckResult {
+    val msg = "Idle connections ${ds.numIdle} [min required is $minIdle]"
+    return if (ds.numIdle >= minIdle) HealthCheckResult.Healthy(msg) else HealthCheckResult.Unhealthy(msg, null)
+  }
+}
