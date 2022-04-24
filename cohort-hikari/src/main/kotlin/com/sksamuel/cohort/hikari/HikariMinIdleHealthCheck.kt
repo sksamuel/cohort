1146be32f8226e207b6cd9f4cf876dc7f0a56880
sksamuel <sam@sksamuel.com>
2022-04-24 11:50:28 -0500

    Added HikariMinIdleHealthCheck

---
+++ cohort-hikari/src/main/kotlin/com/sksamuel/cohort/hikari/HikariMinIdleHealthCheck.kt
@@ -0,0 +1,27 @@
+package com.sksamuel.cohort.hikari
+
+import com.sksamuel.cohort.HealthCheck
+import com.sksamuel.cohort.HealthCheckResult
+import com.zaxxer.hikari.HikariDataSource
+
+/**
+ * A Cohort [HealthCheck] that checks for the number of idle connections in a [HikariDataSource].
+ *
+ * This is useful to detect when connections are being exhausted.
+ *
+ * The check is considered healthy if the number of idle connections >= [minIdle].
+ */
+class HikariMinIdleHealthCheck(
+  private val ds: HikariDataSource,
+  private val minIdle: Int,
+) : HealthCheck {
+  override suspend fun check(): HealthCheckResult {
+    val idleConnections = ds.hikariPoolMXBean.idleConnections
+    val msg = "Idle connections ${idleConnections} [min required is $minIdle]"
+    return if (idleConnections >= minIdle) {
+      HealthCheckResult.Healthy(msg)
+    } else {
+      HealthCheckResult.Unhealthy(msg, null)
+    }
+  }
+}
