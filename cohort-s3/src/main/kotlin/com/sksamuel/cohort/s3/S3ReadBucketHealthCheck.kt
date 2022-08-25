2dd9f46ce31ffaae8607002e3f4265c6fbb9367c
sksamuel <sam@sksamuel.com>
2022-08-25 06:41:32 -0500

    Added s3 read bucket health check

---
+++ cohort-s3/src/main/kotlin/com/sksamuel/cohort/s3/S3ReadBucketHealthCheck.kt
@@ -0,0 +1,20 @@
+package com.sksamuel.cohort.s3
+
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.model.HeadBucketRequest
+import com.sksamuel.cohort.HealthCheck
+import com.sksamuel.cohort.HealthCheckResult
+
+class S3ReadBucketHealthCheck(
+  private val bucketName: String,
+  val createClient: () -> AmazonS3
+) : HealthCheck {
+  override suspend fun check(): HealthCheckResult {
+    return runCatching {
+      createClient().headBucket(HeadBucketRequest(bucketName))
+    }.fold(
+      { HealthCheckResult.Healthy("Connected to bucket $bucketName") },
+      { HealthCheckResult.Unhealthy("Could not connect to bucket $bucketName", it) }
+    )
+  }
+}
