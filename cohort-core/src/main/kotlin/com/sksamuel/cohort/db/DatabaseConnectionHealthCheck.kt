76cfe21dd6b2d6b55204ad09e5cde484aa2f58b3
sksamuel <sam@sksamuel.com>
2022-09-05 20:03:57 -0500

    Added DatabaseConnectionHealthCheck

---
+++ cohort-core/src/main/kotlin/com/sksamuel/cohort/db/DatabaseConnectionHealthCheck.kt
@@ -0,0 +1,26 @@
+package com.sksamuel.cohort.db
+
+import com.sksamuel.cohort.HealthCheck
+import com.sksamuel.cohort.HealthCheckResult
+import javax.sql.DataSource
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
+
+/**
+ * A Cohort [HealthCheck] that checks that a connection can be retrieved from a [DataSource].
+ *
+ * Uses the JDBC4 method isValid(timeout) with the given [timeout] to check that the connection
+ * returned is open and usable.
+ */
+class DatabaseConnectionHealthCheck(
+   private val ds: DataSource,
+   private val timeout: Duration = 1.seconds,
+) : HealthCheck {
+
+   override val name: String = "database_connection"
+
+   override suspend fun check(): HealthCheckResult = ds.connection.use { conn ->
+      conn.isValid(timeout.inWholeSeconds.toInt())
+      HealthCheckResult.Healthy("Connected to database successfully")
+   }
+}
--- cohort-core/src/main/kotlin/com/sksamuel/cohort/db/DatabaseHealthCheck.kt
+++ cohort-core/src/main/kotlin/com/sksamuel/cohort/db/DatabaseHealthCheck.kt
@@ -7,16 +7,19 @@ import javax.sql.DataSource
 /**
  * A [HealthCheck] that checks that a connection can be established with
  * a [DataSource] and a basic query executed.
+ *
+ * @deprecated use DatabaseConnectionHealthCheck which supports the JDBC4 isValid method.
  */
+@Deprecated("Use DatabaseConnectionHealthCheck")
 class DatabaseHealthCheck(
-  private val ds: DataSource,
-  private val query: String = "SELECT 1",
+   private val ds: DataSource,
+   private val query: String = "SELECT 1",
 ) : HealthCheck {
 
-  override val name: String = "database"
+   override val name: String = "database"
 
-  override suspend fun check(): HealthCheckResult = ds.connection.use { conn ->
-    conn.createStatement().executeQuery(query)
-    HealthCheckResult.Healthy("Connected to database successfully")
-  }
+   override suspend fun check(): HealthCheckResult = ds.connection.use { conn ->
+      conn.createStatement().executeQuery(query)
+      HealthCheckResult.Healthy("Connected to database successfully")
+   }
 }
