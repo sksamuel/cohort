2ffbacc8363f9cd57895435f3267bf1e4315c188
sksamuel <sam@sksamuel.com>
2022-09-22 06:16:11 -0500

    Added KafkaClusterWarmupHealthCheck

---
+++ cohort-kafka/src/main/kotlin/com/sksamuel/cohort/kafka/KafkaClusterWarmupHealthCheck.kt
@@ -0,0 +1,25 @@
+package com.sksamuel.cohort.kafka
+
+import com.sksamuel.cohort.HealthCheck
+import com.sksamuel.cohort.WarmupHealthCheck
+import kotlinx.coroutines.future.await
+import org.apache.kafka.clients.admin.AdminClient
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+
+/**
+ * A [HealthCheck] that checks that a connection can be made to a kafka cluster, the controller
+ * can be located, and at least one node is present.
+ */
+class KafkaClusterWarmupHealthCheck(
+   private val adminClient: AdminClient,
+   override val iterations: Int = 1000,
+   override val interval: Duration = 10.milliseconds
+) : WarmupHealthCheck() {
+
+   override val name: String = "kafka_warmup"
+
+   override suspend fun warmup() {
+      adminClient.describeCluster().controller().toCompletionStage().await()
+   }
+}
