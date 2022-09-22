71ab94dad2cbc1bb4d98dd9b40eab8445641a213
sksamuel <sam@sksamuel.com>
2022-09-22 09:02:56 -0500

    Updated http iteration count

--- cohort-http/src/main/kotlin/com/sksamuel/cohort/healthcheck/http/HttpWarmup.kt
+++ cohort-http/src/main/kotlin/com/sksamuel/cohort/healthcheck/http/HttpWarmup.kt
@@ -8,7 +8,7 @@ import kotlin.time.Duration
 import kotlin.time.Duration.Companion.milliseconds
 
 class HttpWarmup(
-   override val iterations: Int = 100,
+   override val iterations: Int = 1000,
    override val interval: Duration = 10.milliseconds,
    private val command: suspend (HttpClient) -> Unit,
 ) : Warmup() {
