5aa5c2f21ac5ce74f1c44f54a6ea166ba7dea8a9
sksamuel <sam@sksamuel.com>
2022-10-10 21:05:11 -0500

    Updated RedisConnectionWarmup sig

--- cohort-lettuce/src/main/kotlin/com/sksamuel/cohort/redis/RedisConnectionWarmup.kt
+++ cohort-lettuce/src/main/kotlin/com/sksamuel/cohort/redis/RedisConnectionWarmup.kt
@@ -1,20 +1,27 @@
 package com.sksamuel.cohort.redis
 
 import com.sksamuel.cohort.Warmup
-import io.lettuce.core.RedisClient
 import io.lettuce.core.api.StatefulRedisConnection
+import kotlinx.coroutines.future.await
 import kotlin.random.Random
 
-class RedisConnectionWarmup(
-   client: RedisClient,
-   private val command: suspend (StatefulRedisConnection<String, String>) -> Unit = {
-      it.sync().get(Random.nextInt().toString())
-   }
+class RedisConnectionWarmup<K, V>(
+   private val conn: StatefulRedisConnection<K, V>,
+   private val command: suspend (StatefulRedisConnection<K, V>) -> Unit,
 ) : Warmup {
 
-   override val name: String = "redis_warmup"
+   companion object {
 
-   private val conn = client.connect()
+      operator fun <K> invoke(conn: StatefulRedisConnection<K, *>, genkey: () -> K): RedisConnectionWarmup<K, *> {
+         return RedisConnectionWarmup(conn) { it.async().get(genkey()).await() }
+      }
+
+      operator fun invoke(conn: StatefulRedisConnection<String, *>): RedisConnectionWarmup<String, *> {
+         return RedisConnectionWarmup(conn) { it.async().get(Random.nextInt().toString()).await() }
+      }
+   }
+
+   override val name: String = "redis_warmup"
 
    override suspend fun warm(iteration: Int) {
       command(conn)
