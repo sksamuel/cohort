b095a0164b151d10eb3a843e7a18b37ffb55d6fb
sksamuel <sam@sksamuel.com>
2022-10-10 22:02:40 -0500

    Increase crypto warmup strength

--- cohort-core/src/main/kotlin/com/sksamuel/cohort/crypto/CryptoWarmup.kt
+++ cohort-core/src/main/kotlin/com/sksamuel/cohort/crypto/CryptoWarmup.kt
@@ -13,10 +13,10 @@ class CryptoWarmup(private val algos: Set<String> = setOf("HmacSHA256", "HmacSHA
 
    override suspend fun warm(iteration: Int) {
       algos.forEach { algorithm ->
-         val secret = Random.nextBytes(32)
-         val str1 = randomAZ(4096)
-         val str2 = randomAZ(4096)
-         val str3 = randomAZ(4096)
+         val secret = Random.nextBytes(4096)
+         val str1 = randomAZ(4096 * 12)
+         val str2 = randomAZ(4096 * 12)
+         val str3 = randomAZ(4096 * 12)
          val mac = Mac.getInstance(algorithm)
          mac.init(SecretKeySpec(secret, algorithm))
          mac.update(str1.encodeToByteArray())
@@ -30,5 +30,6 @@ suspend fun main() {
    val w = CryptoWarmup()
    repeat(1000) {
       w.warm(it)
+      println(it)
    }
 }
