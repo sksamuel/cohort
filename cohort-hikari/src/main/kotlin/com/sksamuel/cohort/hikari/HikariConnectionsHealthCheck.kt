53cbf2ce37c2ebfe785dbc502d778abba7e6ebe5
sksamuel <sam@sksamuel.com>
2023-05-13 15:16:25 -0500

    Updated docs

--- cohort-hikari/src/main/kotlin/com/sksamuel/cohort/hikari/HikariConnectionsHealthCheck.kt
+++ cohort-hikari/src/main/kotlin/com/sksamuel/cohort/hikari/HikariConnectionsHealthCheck.kt
@@ -7,24 +7,25 @@ import com.zaxxer.hikari.HikariDataSource
 /**
  * A Cohort [HealthCheck] that checks for the number of connections in a [HikariDataSource].
  *
- * This is useful to detect when connections are unable to be made.
+ * This is useful to ensure a service has opened a required number of connections before being
+ * considered healthy.
  *
  * The check is considered healthy if the total connection count is >= [minConnections].
  */
 class HikariConnectionsHealthCheck(
-  private val ds: HikariDataSource,
-  private val minConnections: Int,
+   private val ds: HikariDataSource,
+   private val minConnections: Int,
 ) : HealthCheck {
 
-  override val name: String = "hikari_open_connections"
+   override val name: String = "hikari_open_connections"
 
-  override suspend fun check(): HealthCheckResult {
-    val conns = ds.hikariPoolMXBean.totalConnections
-    val msg = "$conns connection(s) to Hikari db-pool ${ds.poolName} [minConnections:$minConnections]"
-    return if (conns >= minConnections) {
-      HealthCheckResult.healthy(msg)
-    } else {
-      HealthCheckResult.unhealthy(msg, null)
-    }
-  }
+   override suspend fun check(): HealthCheckResult {
+      val conns = ds.hikariPoolMXBean.totalConnections
+      val msg = "$conns connection(s) to Hikari db-pool ${ds.poolName} [minConnections:$minConnections]"
+      return if (conns >= minConnections) {
+         HealthCheckResult.healthy(msg)
+      } else {
+         HealthCheckResult.unhealthy(msg, null)
+      }
+   }
 }
