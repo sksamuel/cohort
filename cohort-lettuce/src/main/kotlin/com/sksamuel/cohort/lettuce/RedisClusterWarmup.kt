18374c09dcd4ab2f2f4d69d0776ba51497031d4c
sksamuel <sam@sksamuel.com>
2023-05-13 17:19:11 -0500

    Improved RedisClusterWarmup

--- cohort-lettuce/src/main/kotlin/com/sksamuel/cohort/lettuce/RedisClusterWarmup.kt
+++ cohort-lettuce/src/main/kotlin/com/sksamuel/cohort/lettuce/RedisClusterWarmup.kt
@@ -5,14 +5,28 @@ import io.lettuce.core.cluster.api.StatefulRedisClusterConnection
 import kotlinx.coroutines.future.await
 import kotlin.random.Random
 
+/**
+ * A Redis [WarmupHealthCheck] that uses a supplied Lettuce [StatefulRedisClusterConnection]
+ * to execute commands.
+ *
+ * By default, the [eval] function will set elements with a 1 second TTL under random keys with
+ * the prefix "cohort_warmup". Any chain of commands can be used by providing a custom [eval] function.
+ */
 class RedisClusterWarmup<K, V>(
    private val conn: StatefulRedisClusterConnection<K, V>,
    override val iterations: Int,
-   private val command: suspend (StatefulRedisClusterConnection<K, V>) -> Unit,
+   private val eval: suspend (StatefulRedisClusterConnection<K, V>) -> Unit,
 ) : WarmupHealthCheck() {
 
    companion object {
 
+      @Deprecated(
+         "Provide a custom eval function or use the default", ReplaceWith(
+            "RedisClusterWarmup(conn, iterations) { it.async().get(genkey()).await() }",
+            "com.sksamuel.cohort.lettuce.RedisClusterWarmup",
+            "kotlinx.coroutines.future.await"
+         )
+      )
       operator fun <K> invoke(
          iterations: Int,
          conn: StatefulRedisClusterConnection<K, *>,
@@ -25,13 +39,18 @@ class RedisClusterWarmup<K, V>(
          iterations: Int,
          conn: StatefulRedisClusterConnection<String, *>
       ): RedisClusterWarmup<String, *> {
-         return RedisClusterWarmup(conn, iterations) { it.async().get(Random.nextInt().toString()).await() }
+         return RedisClusterWarmup(conn, iterations) {
+            val key = "cohort_warmup_" + Random.nextInt()
+            it.async().incr(key).await()
+            it.async().expire(key, 1).await()
+            it.async().get(key).await()
+         }
       }
    }
 
    override val name: String = "redis_warmup"
 
    override suspend fun warm(iteration: Int) {
-      command(conn)
+      eval(conn)
    }
 }
