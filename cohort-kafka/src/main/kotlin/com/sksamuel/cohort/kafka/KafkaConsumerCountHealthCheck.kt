e4bb8aba0b5306e20b4dfb59ccf8394bcf623a9f
sksamuel <sam@sksamuel.com>
2023-07-17 07:49:15 -0500

    Added healthyUntilFirstRecord to KafkaConsumerCountHealthCheck

--- cohort-kafka/src/main/kotlin/com/sksamuel/cohort/kafka/KafkaConsumerCountHealthCheck.kt
+++ cohort-kafka/src/main/kotlin/com/sksamuel/cohort/kafka/KafkaConsumerCountHealthCheck.kt
@@ -12,10 +12,14 @@ import org.apache.kafka.clients.consumer.KafkaConsumer
  * This check reports healthy if the records consumed between invocations is >= [min].
  * For example, when you register this check, if you specify a min value of 100, then at least
  * 100 records must be consumed during the interval you specify when you registered the check.
+ *
+ * @param healthyUntilFirstRecord if true, then this healthcheck is healthy while no records have yet been received.
+ * This allows a consumer pod to go healthy first, and only report unhealthy if later throughput stalls.
  */
 class KafkaConsumerCountHealthCheck(
    private val consumer: KafkaConsumer<*, *>,
    private val min: Int,
+   private val healthyUntilFirstRecord: Boolean = false,
 ) : HealthCheck {
 
    private val metricName = "records-consumed-total"
@@ -29,10 +33,10 @@ class KafkaConsumerCountHealthCheck(
       val metric = consumer.metrics().values.firstOrNull { it.metricName().name() == metricName }
          ?: return HealthCheckResult.unhealthy("Could not locate kafka metric '${metricName}'", null)
 
-      val total = metric.metricValue().toString().toLongOrNull() ?: 0
+      val total = metric.metricValue().toString().toLongOrNull() ?: 0L
 
       // first time
-      return if (lastTotal == -1L) {
+      return if (lastTotal == -1L || (healthyUntilFirstRecord && total == 0L)) {
          lastTotal = total
          HealthCheckResult.healthy("Kafka consumer count initial scan")
       } else {
