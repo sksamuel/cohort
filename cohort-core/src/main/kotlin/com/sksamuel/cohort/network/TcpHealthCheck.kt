db0c03d14ee039d6eeaed39ba4d50a97c41cf63e
sksamuel <sam@sksamuel.com>
2023-07-22 18:22:07 -0500

    Formatting

--- cohort-core/src/main/kotlin/com/sksamuel/cohort/network/TcpHealthCheck.kt
+++ cohort-core/src/main/kotlin/com/sksamuel/cohort/network/TcpHealthCheck.kt
@@ -11,25 +11,25 @@ import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Duration.Companion.seconds
 
 class TcpHealthCheck(
-  private val host: String,
-  private val port: Int,
-  private val connectionTimeout: Duration = 4.seconds
+   private val host: String,
+   private val port: Int,
+   private val connectionTimeout: Duration = 4.seconds
 ) : HealthCheck {
 
-  override suspend fun check(): HealthCheckResult {
-    return runCatching {
-      val socket = Socket()
-      val start = System.currentTimeMillis()
-      socket.connect(InetSocketAddress(host, port), connectionTimeout.inWholeMilliseconds.toInt())
-      val time = (System.currentTimeMillis() - start).milliseconds
-      if (socket.isConnected) {
-        withContext(Dispatchers.IO) {
-          socket.close()
-        }
-         HealthCheckResult.healthy("Connected to $host:$port after ${time.inWholeMilliseconds}ms")
-      } else {
-         HealthCheckResult.unhealthy("Connection to $host:$port timed out after $connectionTimeout", null)
-      }
-    }.getOrElse { HealthCheckResult.unhealthy("Connection to $host:$port failed", it) }
-  }
+   override suspend fun check(): HealthCheckResult {
+      return runCatching {
+         val socket = Socket()
+         val start = System.currentTimeMillis()
+         socket.connect(InetSocketAddress(host, port), connectionTimeout.inWholeMilliseconds.toInt())
+         val time = (System.currentTimeMillis() - start).milliseconds
+         if (socket.isConnected) {
+            withContext(Dispatchers.IO) {
+               socket.close()
+            }
+            HealthCheckResult.healthy("Connected to $host:$port after ${time.inWholeMilliseconds}ms")
+         } else {
+            HealthCheckResult.unhealthy("Connection to $host:$port timed out after $connectionTimeout", null)
+         }
+      }.getOrElse { HealthCheckResult.unhealthy("Connection to $host:$port failed", it) }
+   }
 }
