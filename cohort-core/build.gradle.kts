0b7751a2944f8766adf94a3bc57e824cddeb02c6
sksamuel <sam@sksamuel.com>
2023-07-22 12:11:52 -0500

    Added tests for DatabaseConnectionHealthCheck

--- cohort-core/build.gradle.kts
+++ cohort-core/build.gradle.kts
@@ -2,6 +2,7 @@ dependencies {
    implementation(libs.jackson.module.kotlin)
    implementation(libs.ktor.server.host.common)
    testImplementation(libs.log4j2.slf4j2.impl)
+   testImplementation("com.h2database:h2:2.1.214")
 }
 
 apply("../publish.gradle.kts")
---
+++ cohort-core/src/test/kotlin/com/sksamuel/cohort/db/DatabaseConnectionHealthCheckTest.kt
@@ -0,0 +1,35 @@
+package com.sksamuel.cohort.db
+
+import com.sksamuel.cohort.HealthStatus
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class DatabaseConnectionHealthCheckTest : FunSpec({
+
+   test("should return healthy for valid query") {
+      val ds = createHikariDS()
+      ds.connection.use { it.createStatement().executeUpdate("CREATE TABLE foo (id int)") }
+      DatabaseConnectionHealthCheck(
+         ds,
+         query = "select * from foo"
+      ).check().status shouldBe HealthStatus.Healthy
+   }
+
+   test("should return unhealthy for invalid query") {
+      DatabaseConnectionHealthCheck(
+         createHikariDS(),
+         query = "selectfff"
+      ).check().status shouldBe HealthStatus.Unhealthy
+   }
+
+})
+
+fun createHikariDS(): HikariDataSource =
+   HikariConfig().apply {
+      jdbcUrl = "jdbc:h2:mem:kjs;DB_CLOSE_DELAY=-1"
+      username = "sa"
+      password = ""
+      maximumPoolSize = 1
+   }.let { HikariDataSource(it) }
