41e734acb5d6677446a13b1b49102c6a9ddb0a5e
sksamuel <sam@sksamuel.com>
2023-09-06 08:09:18 -0500

    Added logHealthyStatus option to loggers

--- cohort-log4j2/src/main/kotlin/com/sksamuel/cohort/log4j2/Log4j2HealthCheckLogging.kt
+++ cohort-log4j2/src/main/kotlin/com/sksamuel/cohort/log4j2/Log4j2HealthCheckLogging.kt
@@ -2,14 +2,21 @@ package com.sksamuel.cohort.log4j2
 
 import com.sksamuel.cohort.HealthCheck
 import com.sksamuel.cohort.HealthCheckResult
+import com.sksamuel.cohort.HealthStatus
 import com.sksamuel.cohort.Subscriber
 import org.apache.logging.log4j.LogManager
 
-class Log4j2HealthCheckLogging : Subscriber {
+/**
+ * Logs each time a health check is invoked.
+ *
+ * @param logHealthyStatus if true, then log both healthy and unhealthy. If false, only log unhealthy statuses.
+ */
+class Log4j2HealthCheckLogging(private val logHealthyStatus: Boolean = true) : Subscriber {
 
    private val logger = LogManager.getLogger(Log4j2HealthCheckLogging::class.java)
 
    override suspend fun invoke(name: String, check: HealthCheck, result: HealthCheckResult) {
-      logger.info("HealthCheck $name: $result")
+      if (result.status == HealthStatus.Unhealthy || logHealthyStatus)
+         logger.info("HealthCheck $name: $result")
    }
 }
--- cohort-logback/src/main/kotlin/com/sksamuel/cohort/logback/LogbackHealthCheckLogging.kt
+++ cohort-logback/src/main/kotlin/com/sksamuel/cohort/logback/LogbackHealthCheckLogging.kt
@@ -2,14 +2,21 @@ package com.sksamuel.cohort.logback
 
 import com.sksamuel.cohort.HealthCheck
 import com.sksamuel.cohort.HealthCheckResult
+import com.sksamuel.cohort.HealthStatus
 import com.sksamuel.cohort.Subscriber
 import org.slf4j.LoggerFactory
 
-class LogbackHealthCheckLogging : Subscriber {
+/**
+ * Logs each time a health check is invoked.
+ *
+ * @param logHealthyStatus if true, then log both healthy and unhealthy. If false, only log unhealthy statuses.
+ */
+class LogbackHealthCheckLogging(private val logHealthyStatus: Boolean = true) : Subscriber {
 
    private val logger = LoggerFactory.getLogger(LogbackHealthCheckLogging::class.java)
 
    override suspend fun invoke(name: String, check: HealthCheck, result: HealthCheckResult) {
-      logger.info("HealthCheck $name: $result")
+      if (result.status == HealthStatus.Unhealthy || logHealthyStatus)
+         logger.info("HealthCheck $name: $result")
    }
 }
