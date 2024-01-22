86c566e021f127a2fce7d6b7fd2f89399bfde51e
monosoul <Kloz.Klaud@gmail.com>
2024-01-22 12:05:52 +0100

    fix(MongoConnectionHealthCheck): make the health check usable with both sync and async clients

--- cohort-mongo/build.gradle.kts
+++ cohort-mongo/build.gradle.kts
@@ -1,7 +1,8 @@
 dependencies {
    implementation(projects.cohortApi)
-   api(libs.mongodb.driver.sync)
+   compileOnly(libs.mongodb.driver.sync)
    testImplementation(libs.testcontainers.mongodb)
+   testImplementation(libs.mongodb.driver.sync)
 }
 
 apply("../publish.gradle.kts")
--- cohort-mongo/src/main/kotlin/com/sksamuel/cohort/mongo/MongoConnectionHealthCheck.kt
+++ cohort-mongo/src/main/kotlin/com/sksamuel/cohort/mongo/MongoConnectionHealthCheck.kt
@@ -2,27 +2,21 @@ package com.sksamuel.cohort.mongo
 
 import com.mongodb.client.MongoClient
 import com.sksamuel.cohort.HealthCheck
-import com.sksamuel.cohort.HealthCheckResult
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runInterruptible
-import kotlinx.coroutines.withTimeout
-import kotlin.time.Duration.Companion.seconds
 
-class MongoConnectionHealthCheck(
-   private val client: MongoClient,
-   override val name: String = "mongo_connection",
-) : HealthCheck {
+class MongoConnectionHealthCheck private constructor(
+   private val delegate: GenericMongoConnectionHealthCheck,
+) : HealthCheck by delegate {
 
-  override suspend fun check(): HealthCheckResult {
-    return runCatching {
-      withTimeout(5.seconds) {
-        runInterruptible(Dispatchers.IO) {
-          val dbs = client.listDatabaseNames().toList()
-          HealthCheckResult.healthy("Connected to mongo instance (${dbs.size} databases)")
-        }
+   constructor(
+      client: MongoClient,
+      name: String = "mongo_connection",
+   ) : this(
+      GenericMongoConnectionHealthCheck(name) {
+         runInterruptible(Dispatchers.IO) {
+            client.listDatabaseNames().toList()
+         }
       }
-    }.getOrElse {
-      HealthCheckResult.unhealthy("Could not connect to mongo instance", it)
-    }
-  }
+   )
 }
