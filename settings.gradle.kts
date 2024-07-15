f9edc86806a380c09e690885f1eae30f738d8190
sksamuel <sam@sksamuel.com>
2024-07-15 00:13:36 -0500

    Bumped vertx

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -129,7 +129,7 @@ dependencyResolutionManagement {
          library("testcontainers-mongodb", "org.testcontainers:mongodb:$testContainers")
          library("testcontainers-kafka", "org.testcontainers:kafka:$testContainers")
 
-         val vertx = "4.5.7"
+         val vertx = "4.5.8"
          library("vertx-core", "io.vertx:vertx-core:$vertx")
          library("vertx-web", "io.vertx:vertx-web:$vertx")
          library("vertx-kotlin", "io.vertx:vertx-lang-kotlin:$vertx")
