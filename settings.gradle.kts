3d059a5bf9803d6c40270daa1c8400a661199f9e
monosoul <andrei.nevedomskii@wolt.com>
2024-10-14 14:15:43 +0200

    Use foojay plugin to resolve toolchains automatically

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,6 +27,8 @@ include(
 enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
+plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0" }
+
 dependencyResolutionManagement {
    versionCatalogs {
       create("libs") {
