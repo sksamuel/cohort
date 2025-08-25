2d3991ce6f75602190aedeccfdcce50b3e143472
sksamuel <sam@sksamuel.com>
2025-08-25 12:48:10 -0500

    Refactor `HealthCheckRegistryTest` for improved readability and stability:
    - Use `AtomicBoolean` for `SlowHealthCheck` initialization.
    - Introduce `eventually` for non-deterministic assertions.
    - Adjust delays and refine test assertions for better clarity.

--- cohort-api/src/test/kotlin/com/sksamuel/cohort/HealthCheckRegistryTest.kt
+++ cohort-api/src/test/kotlin/com/sksamuel/cohort/HealthCheckRegistryTest.kt
@@ -1,15 +1,18 @@
 package com.sksamuel.cohort
 
 import com.sksamuel.cohort.threads.ThreadDeadlockHealthCheck
+import io.kotest.assertions.nondeterministic.eventually
 import io.kotest.assertions.throwables.shouldThrowAny
 import io.kotest.assertions.withClue
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.inspectors.forAll
 import io.kotest.matchers.comparables.shouldBeGreaterThan
 import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.delay
 import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.atomic.AtomicLong
 import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
 
 class HealthCheckRegistryTest : FunSpec({
 
@@ -22,14 +25,14 @@ class HealthCheckRegistryTest : FunSpec({
       }
    }
 
-   class SlowHealthCheck(val id: String, val blocker: AtomicBoolean = AtomicBoolean(true),
-                         val callCount: AtomicLong = AtomicLong(0),
-
+   class SlowHealthCheck(
+      val blocker: AtomicBoolean,
+      val callCount: AtomicLong = AtomicLong(0),
    ) : HealthCheck {
       override suspend fun check(): HealthCheckResult {
          callCount.incrementAndGet()
-         while(blocker.get()) {
-            Thread.sleep(1)
+         while (blocker.get()) {
+            Thread.sleep(25)
          }
          return HealthCheckResult.healthy("Slow")
       }
@@ -37,14 +40,15 @@ class HealthCheckRegistryTest : FunSpec({
 
 
    test("slow blocking check should not hold up existing checks") {
-       val check1 = SlowHealthCheck("1").apply { blocker.set(false) }
-       val check2 = SlowHealthCheck("2")
-       val check3 = SlowHealthCheck("3").apply { blocker.set(false) }
+
+      val check1 = SlowHealthCheck(AtomicBoolean(false))
+      val check2 = SlowHealthCheck(AtomicBoolean(true))
+      val check3 = SlowHealthCheck(AtomicBoolean(false))
 
       val reg = HealthCheckRegistry {
          register("check1", check1, 1.milliseconds, 1.milliseconds)
-         register("check2", check2, 1.milliseconds,  1.milliseconds)
-         register("check3", check3,  1.milliseconds,  1.milliseconds)
+         register("check2", check2, 1.milliseconds, 1.milliseconds)
+         register("check3", check3, 1.milliseconds, 1.milliseconds)
       }
 
       delay(5)
@@ -53,24 +57,31 @@ class HealthCheckRegistryTest : FunSpec({
          this.healthy shouldBe false
          this.healthchecks.size shouldBe 3
 
+         this.healthchecks["check1"]?.result?.isHealthy shouldBe true
          this.healthchecks["check2"]?.result?.isHealthy shouldBe false
-         withClue("Check 2 should still be waiting for first run to finish") { check2.callCount.get() shouldBe 1 }
          this.healthchecks["check3"]?.result?.isHealthy shouldBe true
       }
+
+      delay(5)
+
+      withClue("Check 2 should still be waiting for first run to finish") {
+         check2.callCount.get() shouldBe 1
+      }
+
+      check1.callCount.get() shouldBeGreaterThan 1
+      check1.callCount.get() shouldBeGreaterThan 1
 
       //Unblock the slow 2nd check
       check2.blocker.set(false)
 
-      delay(3)
+      eventually(1.seconds) {
+         reg.status().apply {
+            this.healthy shouldBe true
+            this.healthchecks.size shouldBe 3
 
-      reg.status().apply {
-         this.healthy shouldBe true
-         this.healthchecks.size shouldBe 3
-
-         this.healthchecks.all { it.value.result.isHealthy shouldBe true }
-         check2.callCount.get() shouldBeGreaterThan 1
+            this.healthchecks.forAll { it.value.result.isHealthy shouldBe true }
+            check2.callCount.get() shouldBeGreaterThan 1
+         }
       }
-
    }
-
 })
